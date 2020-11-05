ea2e18a868d2fae09efce19cd1aaa466bf16a312
Aleksandr <shoppingplaneta@gmail.com>
2020-11-05 15:27:45 +0200

    done tabs.js

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,48 @@
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+  const header = document.querySelector(headerSelector),
+        tab = document.querySelectorAll(tabSelector),
+        content = document.querySelectorAll(contentSelector);
+
+  function hideTabContent() {
+    content.forEach(item => {
+      item.style.cssText = `
+        display: none;
+      `;
+    });
+
+    tab.forEach(item => {
+      item.classList.remove(activeClass);
+    });
+  } //end hideTabContent
+
+  function showTabContent(i = 0) {
+    content[i].style.cssText = `
+    display: block;
+    animation-name: fade;
+    animation-duration: 3s;
+  `;
+
+    tab[i].classList.add(activeClass);
+  } //end showTabContent
+
+  hideTabContent();
+  showTabContent();
+
+
+  //Which tabs was click user
+  header.addEventListener('click', (e) => {
+    const target = e.target;
+    if(target &&
+      (target.classList.contains(tabSelector.replace(/\./, "")) ||
+    target.parentNode.classList.contains(tabSelector.replace(/\./, "")))) {
+      tab.forEach((item, i) => {
+        if(target == item || target.parentNode == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  }); //end click
+};
+
+export default tabs;
