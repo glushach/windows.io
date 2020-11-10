f3fb5ccedb8f9753edeac5dc5411f04916ce17ef
Aleksandr <shoppingplaneta@gmail.com>
2020-11-10 16:44:31 +0200

    done module timer

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import modals from "./modules/modals";
 import tabs from "./modules/tabs";
 import forms from "./modules/forms";
 import changeModalState from "./modules/changeModalState";
+import timer from "./modules/timer";
 
 window.addEventListener('DOMContentLoaded', ()=>{
   "use strict";
@@ -10,9 +11,12 @@ window.addEventListener('DOMContentLoaded', ()=>{
   let modalState = {};
   changeModalState(modalState);
 
+  let deadline = '2020-12-31';
+
   modals();
   tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
   tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
   tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
   forms(modalState);
+  timer('.container1', deadline);
 });
